86730d14d4db491911d31ca08625f44f213da5f9
yetanotherpassword <alcampton@gmail.com>
2021-09-05 01:13:36 +1000

    update - think it works, very close (maybe not calc bias re weighting?)

--- armo.cpp
+++ armo.cpp
@@ -59,17 +59,19 @@ rowvec expected_answer = { 0.01, 0.99};
 rowvec AOnes={1,1};
 rowvec n1, n2;
 rowvec a1, a2;
-rowvec delta1;
-rowvec ftick1;
+rowvec delta1, delta2;
+rowvec ftick1, ftick2;
 
 mat updates1(2,2);
+mat updates2(2,2);
 mat new_weights1(2,2);
+mat new_weights2(2,2);
 mat L1 = {{   0.15, 0.25},   // L1N1 weights for I1 (col 1), I2 (col 2)and Bias
        {0.2, 0.3}};   
 
 mat Loutput ={{  0.4, 0.5}, 
                {0.45, 0.55}};
-
+forward_feed:
 cout << "------------------------------------ Layer1 weights" << endl;
 std::cout << L1 << std::endl;
 cout << "------------------------------------ Output weights" << endl;
@@ -97,6 +99,9 @@ std::cout << n2<< std::endl;
 cout << "------------------------------------ Activation out of Output Layer" << endl;
 a2=sigmoid(n2);
 std::cout << a2<< std::endl;
+//////////////////////////// forward feed end
+
+
 
 cout << "------------------------------------ BACKPROPAGATION" << endl;
 cout << "------------------------------------ Delta of Output Layer" << endl;
@@ -112,5 +117,31 @@ std::cout << updates1<< std::endl;
 cout << "------------------------------------ New updates are at Output Layer" << endl;
 new_weights1 = Loutput + (eta *  updates1.t()) ;
 std::cout << new_weights1 << std::endl;
+
+here:
+cout << "------------------------------------ Delta of Hidden Layer" << endl;
+ftick2=(AOnes-Linput);
+ftick2=ftick2 % (Linput);  //element wise multiply
+delta2=delta1*L1;
+delta2=delta2 % ftick2;
+std::cout << delta2<< std::endl;
+//std::cout << ftick2<< std::endl;
+
+cout << "------------------------------------ Updates to weights at Hidden Layer" << endl;
+updates2 = delta2.t() * a1;
+std::cout << updates2<< std::endl;
+
+cout << "------------------------------------ New updates are at Hidden Layer" << endl;
+new_weights2 = L1 + (eta *  updates2.t()) ;
+std::cout << new_weights2 << std::endl;
+
+delta1=delta2;  // if there was another layer ; then goto here:, with L1 and Linput decremented
+// then when hit input layer break, and do
+
+L1 = new_weights1;
+Loutput = new_weights2;
+
+// goto forward_feed:
+
 
 }
