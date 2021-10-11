4b584137ea31a8118f8eda12ffb69b66d8fc8271
Allan Campton <s4604901@fac-login-1.local>
2021-10-12 01:21:21 +1000

    updates

--- avx.sh
+++ avx.sh
@@ -3,11 +3,15 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --time=0-10:00
-#SBATCH --constraint=R640|R740|FC430
+#SBATCH --constraint=R640
+#SBATCH --mem-per-cpu=4000M
+##SBATCH --constraint=R640|R740|FC430
 ##SBATCH --cpus-per-task=24
 ##SBATCH --ntasks-per-node=1
 ##SBATCH --mem-per-cpu=1G
 echo $HOSTNAME
+echo "smp-7-2 R640"
+echo "smp-9-2 FC430"
 lscpu
 export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
 export MKL_NUM_THREADS=${SLURM_CPUS_PER_TASK}
@@ -18,22 +22,137 @@ echo $SLURM_NODELIST
 echo "running with SLURM_TASKS_PER_NODE= $SLURM_TASKS_PER_NODE "
 echo "Running Command \"$@\" "
 echo ${SLURM_TMPDIR}
-ls ${SLURM_TMPDIR}
+#var/spool/slurmd/job11076110/slurm_script
 date
 echo "-----------------------------------------------------------------------------"
-echo ./Assignment2_serial 30000
-./Assignment2_serial 30000
+echo ./Assignment2_serial 5000
+./Assignment2_serial 5000
 echo "+++++++++++++++++++"
-echo ./Assignment2_avx 30000
-./Assignment2_avx 30000
-echo ./Assignment2_avx 30000
-./Assignment2_avx 30000
-echo ./Assignment2_avx 30000
-./Assignment2_avx 30000
-echo ./Assignment2_avx 30000
-./Assignment2_avx 30000
-echo ./Assignment2_avx 30000
-./Assignment2_avx 30000
+echo ./Assignment2_avx 5000
+./Assignment2_avx 5000
+echo "+++++++++++++++++++"
+echo ./Assignment2_avx 5000
+./Assignment2_avx 5000
+echo "+++++++++++++++++++"
+echo ./Assignment2_avx 5000
+./Assignment2_avx 5000
+echo "+++++++++++++++++++"
+echo ./Assignment2_avx 5000
+./Assignment2_avx 5000
+echo "+++++++++++++++++++"
+echo ./Assignment2_avx 5000
+./Assignment2_avx 5000
+echo "-----------------------------------------------------------------------------"
+echo ./Assignment2_serial 1000
+./Assignment2_serial 1000
+echo "+++++++++++++++++++"
+echo ./Assignment2_avx 1000
+./Assignment2_avx 1000
+echo "+++++++++++++++++++"
+echo ./Assignment2_avx 1000
+./Assignment2_avx 1000
+echo "+++++++++++++++++++"
+echo ./Assignment2_avx 1000
+./Assignment2_avx 1000
+echo "+++++++++++++++++++"
+echo ./Assignment2_avx 1000
+./Assignment2_avx 1000
+echo "+++++++++++++++++++"
+echo ./Assignment2_avx 1000
+./Assignment2_avx 1000
+echo "-----------------------------------------------------------------------------"
+echo ./Assignment2_serial 100
+./Assignment2_serial 100
+echo "+++++++++++++++++++"
+echo ./Assignment2_avx 100
+./Assignment2_avx 100
+echo "+++++++++++++++++++"
+echo ./Assignment2_avx 100
+./Assignment2_avx 100
+echo "+++++++++++++++++++"
+echo ./Assignment2_avx 100
+./Assignment2_avx 100
+echo "+++++++++++++++++++"
+echo ./Assignment2_avx 100
+./Assignment2_avx 100
+echo "+++++++++++++++++++"
+echo ./Assignment2_avx 100
+./Assignment2_avx 100
+echo "-----------------------------------------------------------------------------"
+echo ./Assignment2_serial 10
+./Assignment2_serial 10
+echo "+++++++++++++++++++"
+echo ./Assignment2_avx 10
+./Assignment2_avx 10
+echo "+++++++++++++++++++"
+echo ./Assignment2_avx 10
+./Assignment2_avx 10
+echo "+++++++++++++++++++"
+echo ./Assignment2_avx 10
+./Assignment2_avx 10
+echo "+++++++++++++++++++"
+echo ./Assignment2_avx 10
+./Assignment2_avx 10
+echo "+++++++++++++++++++"
+echo ./Assignment2_avx 10
+./Assignment2_avx 10
+echo "-----------------------------------------------------------------------------"
+echo ./Assignment2_serial 6000
+./Assignment2_serial 6000
+echo "+++++++++++++++++++"
+echo ./Assignment2_avx 6000
+./Assignment2_avx 6000
+echo "+++++++++++++++++++"
+echo ./Assignment2_avx 6000
+./Assignment2_avx 6000
+echo "+++++++++++++++++++"
+echo ./Assignment2_avx 6000
+./Assignment2_avx 6000
+echo "+++++++++++++++++++"
+echo ./Assignment2_avx 6000
+./Assignment2_avx 6000
+echo "+++++++++++++++++++"
+echo ./Assignment2_avx 6000
+./Assignment2_avx 6000
+echo "-----------------------------------------------------------------------------"
+echo ./Assignment2_serial 7500
+./Assignment2_serial 7500
+echo "+++++++++++++++++++"
+echo ./Assignment2_avx 7500
+./Assignment2_avx 7500
+echo "+++++++++++++++++++"
+echo ./Assignment2_avx 7500
+./Assignment2_avx 7500
+echo "+++++++++++++++++++"
+echo ./Assignment2_avx 7500
+./Assignment2_avx 7500
+echo "+++++++++++++++++++"
+echo ./Assignment2_avx 7500
+./Assignment2_avx 7500
+echo "+++++++++++++++++++"
+echo ./Assignment2_avx 7500
+./Assignment2_avx 7500
+echo "+++++++++++++++++++"
+echo "-----------------------------------------------------------------------------"
+echo "-----------------------------------------------------------------------------"
+echo ./Assignment2_serial 10000
+./Assignment2_serial 10000
+echo "+++++++++++++++++++"
+echo ./Assignment2_avx 10000
+./Assignment2_avx 10000
+echo "+++++++++++++++++++"
+echo ./Assignment2_avx 10000
+./Assignment2_avx 10000
+echo "+++++++++++++++++++"
+echo ./Assignment2_avx 10000
+./Assignment2_avx 10000
+echo "+++++++++++++++++++"
+echo ./Assignment2_avx 10000
+./Assignment2_avx 10000
+echo "+++++++++++++++++++"
+echo ./Assignment2_avx 10000
+./Assignment2_avx 10000
+echo "-----------------------------------------------------------------------------"
 echo "-----------------------------------------------------------------------------"
 echo ./Assignment2_serial 20000
 ./Assignment2_serial 20000
@@ -49,58 +168,21 @@ echo ./Assignment2_avx 20000
 echo ./Assignment2_avx 20000
 ./Assignment2_avx 20000
 echo "-----------------------------------------------------------------------------"
-echo ./Assignment2_serial 10000
-./Assignment2_serial 10000
+echo ./Assignment2_serial 30000
+./Assignment2_serial 30000
 echo "+++++++++++++++++++"
-echo ./Assignment2_avx 10000
-./Assignment2_avx 10000
-echo ./Assignment2_avx 10000
-./Assignment2_avx 10000
-echo ./Assignment2_avx 10000
-./Assignment2_avx 10000
-echo ./Assignment2_avx 10000
-./Assignment2_avx 10000
-echo ./Assignment2_avx 10000
-./Assignment2_avx 10000
+echo ./Assignment2_avx 30000
+./Assignment2_avx 30000
+echo "+++++++++++++++++++"
+echo ./Assignment2_avx 30000
+./Assignment2_avx 30000
+echo "+++++++++++++++++++"
+echo ./Assignment2_avx 30000
+./Assignment2_avx 30000
+echo "+++++++++++++++++++"
+echo ./Assignment2_avx 30000
+./Assignment2_avx 30000
+echo "+++++++++++++++++++"
+echo ./Assignment2_avx 30000
+./Assignment2_avx 30000
 echo "-----------------------------------------------------------------------------"
-echo ./Assignment2_serial 1000
-./Assignment2_serial 1000
-echo "+++++++++++++++++++"
-echo ./Assignment2_avx 1000
-./Assignment2_avx 1000
-echo ./Assignment2_avx 1000
-./Assignment2_avx 1000
-echo ./Assignment2_avx 1000
-./Assignment2_avx 1000
-echo ./Assignment2_avx 1000
-./Assignment2_avx 1000
-echo ./Assignment2_avx 1000
-./Assignment2_avx 1000
-echo "-----------------------------------------------------------------------------"
-echo ./Assignment2_serial 100
-./Assignment2_serial 100
-echo "+++++++++++++++++++"
-echo ./Assignment2_avx 100
-./Assignment2_avx 100
-echo ./Assignment2_avx 100
-./Assignment2_avx 100
-echo ./Assignment2_avx 100
-./Assignment2_avx 100
-echo ./Assignment2_avx 100
-./Assignment2_avx 100
-echo ./Assignment2_avx 100
-./Assignment2_avx 100
-echo "-----------------------------------------------------------------------------"
-echo ./Assignment2_serial 10
-./Assignment2_serial 10
-echo "+++++++++++++++++++"
-echo ./Assignment2_avx 10
-./Assignment2_avx 10
-echo ./Assignment2_avx 10
-./Assignment2_avx 10
-echo ./Assignment2_avx 10
-./Assignment2_avx 10
-echo ./Assignment2_avx 10
-./Assignment2_avx 10
-echo ./Assignment2_avx 10
-./Assignment2_avx 10
