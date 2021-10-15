d51cfd42e22f0735b28007566a44fd0aaba54455
Allan Campton <s4604901@fac-login-1.local>
2021-10-16 01:29:22 +1000

    new cuda script for slurm

---
+++ cuda16.sh
@@ -0,0 +1,193 @@
+#!/bin/bash
+#SBATCH --job-name=Cuda16
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+##SBATCH  --nodelist=smp-6-3
+#SBATCH --time=0-10:00
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+date
+module load cuda/10.1 gcc
+echo $HOSTNAME
+lscpu
+echo "---------------------------------------------------1"
+echo ./Assignment2_serial 10 
+./Assignment2_serial 10 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda16 10 
+./Assignment2_cuda16 10 
+echo "+++++++++++++++"
+echo ./Assignment2_serial 100 
+echo "+++++++++++++++"
+./Assignment2_serial 100 
+echo ./Assignment2_cuda16 100
+./Assignment2_cuda16 100
+echo "+++++++++++++++"
+echo ./Assignment2_serial 1000 
+./Assignment2_serial 1000 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda16 1000
+./Assignment2_cuda16 1000
+echo "+++++++++++++++"
+echo ./Assignment2_serial 10000 
+./Assignment2_serial 10000 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda16 10000
+./Assignment2_cuda16 10000
+echo "+++++++++++++++"
+echo ./Assignment2_serial 20000 
+./Assignment2_serial 20000 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda16 20000
+./Assignment2_cuda16 20000
+echo "+++++++++++++++"
+echo ./Assignment2_serial 30000 
+./Assignment2_serial 30000 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda16 30000
+./Assignment2_cuda16 30000
+echo "---------------------------------------------------1"
+echo ./Assignment2_serial 10 
+./Assignment2_serial 10 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda16 10 
+./Assignment2_cuda16 10 
+echo "+++++++++++++++"
+echo ./Assignment2_serial 100 
+./Assignment2_serial 100 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda16 100
+./Assignment2_cuda16 100
+echo "+++++++++++++++"
+echo ./Assignment2_serial 1000 
+./Assignment2_serial 1000 
+echo ./Assignment2_cuda16 1000
+./Assignment2_cuda16 1000
+echo "+++++++++++++++"
+echo ./Assignment2_serial 10000 
+./Assignment2_serial 10000 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda16 10000
+./Assignment2_cuda16 10000
+echo "+++++++++++++++"
+echo ./Assignment2_serial 20000 
+./Assignment2_serial 20000 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda16 20000
+./Assignment2_cuda16 20000
+echo "+++++++++++++++"
+echo ./Assignment2_serial 30000 
+./Assignment2_serial 30000 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda16 30000
+./Assignment2_cuda16 30000
+echo "---------------------------------------------------1"
+echo ./Assignment2_serial 10 
+./Assignment2_serial 10 
+echo ./Assignment2_cuda16 10 
+echo "+++++++++++++++"
+./Assignment2_cuda16 10 
+echo "+++++++++++++++"
+echo ./Assignment2_serial 100 
+./Assignment2_serial 100 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda16 100
+./Assignment2_cuda16 100
+echo "+++++++++++++++"
+echo ./Assignment2_serial 1000 
+./Assignment2_serial 1000 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda16 1000
+./Assignment2_cuda16 1000
+echo "+++++++++++++++"
+echo ./Assignment2_serial 10000 
+./Assignment2_serial 10000 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda16 10000
+./Assignment2_cuda16 10000
+echo "+++++++++++++++"
+echo ./Assignment2_serial 20000 
+./Assignment2_serial 20000 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda16 20000
+./Assignment2_cuda16 20000
+echo "+++++++++++++++"
+echo ./Assignment2_serial 30000 
+./Assignment2_serial 30000 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda16 30000
+./Assignment2_cuda16 30000
+echo "---------------------------------------------------1"
+echo ./Assignment2_serial 10 
+./Assignment2_serial 10 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda16 10 
+./Assignment2_cuda16 10 
+echo "+++++++++++++++"
+echo ./Assignment2_serial 100 
+./Assignment2_serial 100 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda16 100
+./Assignment2_cuda16 100
+echo "+++++++++++++++"
+echo ./Assignment2_serial 1000 
+./Assignment2_serial 1000 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda16 1000
+./Assignment2_cuda16 1000
+echo "+++++++++++++++"
+echo ./Assignment2_serial 10000 
+./Assignment2_serial 10000 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda16 10000
+./Assignment2_cuda16 10000
+echo "+++++++++++++++"
+echo ./Assignment2_serial 20000 
+./Assignment2_serial 20000 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda16 20000
+./Assignment2_cuda16 20000
+echo "+++++++++++++++"
+echo ./Assignment2_serial 30000 
+./Assignment2_serial 30000 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda16 30000
+./Assignment2_cuda16 30000
+echo "---------------------------------------------------1"
+echo ./Assignment2_serial 10 
+./Assignment2_serial 10 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda16 10 
+./Assignment2_cuda16 10 
+echo "+++++++++++++++"
+echo ./Assignment2_serial 100 
+./Assignment2_serial 100 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda16 100
+./Assignment2_cuda16 100
+echo "+++++++++++++++"
+echo ./Assignment2_serial 1000 
+./Assignment2_serial 1000 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda16 1000
+./Assignment2_cuda16 1000
+echo "+++++++++++++++"
+echo ./Assignment2_serial 10000 
+./Assignment2_serial 10000 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda16 10000
+./Assignment2_cuda16 10000
+echo "+++++++++++++++"
+echo ./Assignment2_serial 20000 
+./Assignment2_serial 20000 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda16 20000
+./Assignment2_cuda16 20000
+echo "+++++++++++++++"
+echo ./Assignment2_serial 30000 
+./Assignment2_serial 30000 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda16 30000
+./Assignment2_cuda16 30000
+echo "+++++++++++++++"
---
+++ cuda32.sh
@@ -0,0 +1,193 @@
+#!/bin/bash
+#SBATCH --job-name=Cuda32
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+##SBATCH  --nodelist=smp-6-3
+#SBATCH --time=0-10:00
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+date
+module load cuda/10.1 gcc
+echo $HOSTNAME
+lscpu
+echo "---------------------------------------------------1"
+echo ./Assignment2_serial 10 
+./Assignment2_serial 10 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda32 10 
+./Assignment2_cuda32 10 
+echo "+++++++++++++++"
+echo ./Assignment2_serial 100 
+echo "+++++++++++++++"
+./Assignment2_serial 100 
+echo ./Assignment2_cuda32 100
+./Assignment2_cuda32 100
+echo "+++++++++++++++"
+echo ./Assignment2_serial 1000 
+./Assignment2_serial 1000 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda32 1000
+./Assignment2_cuda32 1000
+echo "+++++++++++++++"
+echo ./Assignment2_serial 10000 
+./Assignment2_serial 10000 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda32 10000
+./Assignment2_cuda32 10000
+echo "+++++++++++++++"
+echo ./Assignment2_serial 20000 
+./Assignment2_serial 20000 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda32 20000
+./Assignment2_cuda32 20000
+echo "+++++++++++++++"
+echo ./Assignment2_serial 30000 
+./Assignment2_serial 30000 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda32 30000
+./Assignment2_cuda32 30000
+echo "---------------------------------------------------1"
+echo ./Assignment2_serial 10 
+./Assignment2_serial 10 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda32 10 
+./Assignment2_cuda32 10 
+echo "+++++++++++++++"
+echo ./Assignment2_serial 100 
+./Assignment2_serial 100 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda32 100
+./Assignment2_cuda32 100
+echo "+++++++++++++++"
+echo ./Assignment2_serial 1000 
+./Assignment2_serial 1000 
+echo ./Assignment2_cuda32 1000
+./Assignment2_cuda32 1000
+echo "+++++++++++++++"
+echo ./Assignment2_serial 10000 
+./Assignment2_serial 10000 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda32 10000
+./Assignment2_cuda32 10000
+echo "+++++++++++++++"
+echo ./Assignment2_serial 20000 
+./Assignment2_serial 20000 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda32 20000
+./Assignment2_cuda32 20000
+echo "+++++++++++++++"
+echo ./Assignment2_serial 30000 
+./Assignment2_serial 30000 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda32 30000
+./Assignment2_cuda32 30000
+echo "---------------------------------------------------1"
+echo ./Assignment2_serial 10 
+./Assignment2_serial 10 
+echo ./Assignment2_cuda32 10 
+echo "+++++++++++++++"
+./Assignment2_cuda32 10 
+echo "+++++++++++++++"
+echo ./Assignment2_serial 100 
+./Assignment2_serial 100 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda32 100
+./Assignment2_cuda32 100
+echo "+++++++++++++++"
+echo ./Assignment2_serial 1000 
+./Assignment2_serial 1000 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda32 1000
+./Assignment2_cuda32 1000
+echo "+++++++++++++++"
+echo ./Assignment2_serial 10000 
+./Assignment2_serial 10000 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda32 10000
+./Assignment2_cuda32 10000
+echo "+++++++++++++++"
+echo ./Assignment2_serial 20000 
+./Assignment2_serial 20000 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda32 20000
+./Assignment2_cuda32 20000
+echo "+++++++++++++++"
+echo ./Assignment2_serial 30000 
+./Assignment2_serial 30000 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda32 30000
+./Assignment2_cuda32 30000
+echo "---------------------------------------------------1"
+echo ./Assignment2_serial 10 
+./Assignment2_serial 10 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda32 10 
+./Assignment2_cuda32 10 
+echo "+++++++++++++++"
+echo ./Assignment2_serial 100 
+./Assignment2_serial 100 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda32 100
+./Assignment2_cuda32 100
+echo "+++++++++++++++"
+echo ./Assignment2_serial 1000 
+./Assignment2_serial 1000 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda32 1000
+./Assignment2_cuda32 1000
+echo "+++++++++++++++"
+echo ./Assignment2_serial 10000 
+./Assignment2_serial 10000 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda32 10000
+./Assignment2_cuda32 10000
+echo "+++++++++++++++"
+echo ./Assignment2_serial 20000 
+./Assignment2_serial 20000 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda32 20000
+./Assignment2_cuda32 20000
+echo "+++++++++++++++"
+echo ./Assignment2_serial 30000 
+./Assignment2_serial 30000 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda32 30000
+./Assignment2_cuda32 30000
+echo "---------------------------------------------------1"
+echo ./Assignment2_serial 10 
+./Assignment2_serial 10 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda32 10 
+./Assignment2_cuda32 10 
+echo "+++++++++++++++"
+echo ./Assignment2_serial 100 
+./Assignment2_serial 100 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda32 100
+./Assignment2_cuda32 100
+echo "+++++++++++++++"
+echo ./Assignment2_serial 1000 
+./Assignment2_serial 1000 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda32 1000
+./Assignment2_cuda32 1000
+echo "+++++++++++++++"
+echo ./Assignment2_serial 10000 
+./Assignment2_serial 10000 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda32 10000
+./Assignment2_cuda32 10000
+echo "+++++++++++++++"
+echo ./Assignment2_serial 20000 
+./Assignment2_serial 20000 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda32 20000
+./Assignment2_cuda32 20000
+echo "+++++++++++++++"
+echo ./Assignment2_serial 30000 
+./Assignment2_serial 30000 
+echo "+++++++++++++++"
+echo ./Assignment2_cuda32 30000
+./Assignment2_cuda32 30000
+echo "+++++++++++++++"
