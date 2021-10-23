36464672f8159ab1fa9b9aa2e5f3b8c889265bbe
Mr Allan Campton <s4604901@goliath.labs.eait.uq.edu.au>
2021-10-23 12:26:28 +1000

    remove * in comment for easy cut/paste

--- ann_mnist_digits.cpp
+++ ann_mnist_digits.cpp
@@ -35,24 +35,24 @@
  *
  * To perform a full build and run from scratch, do the following
  *
- *    git clone git@github.com:yetanotherpassword/cosc3500 --branch="Milestone1"
- *    cd ~/cosc3500/
- *    unzip mnist.zip
- *    unxz armadillo-10.6.2.tar.xz
- *    tar xvf armadillo-10.6.2.tar
- *    cd armadillo-10.6.2/
- *    	#Made lib static and issue with MKL on Centos
- *    	#Below changes done in my git, but may need to do if download from
- *    	#http://sourceforge.net/projects/arma/files/armadillo-10.6.2.tar.xz
- *    	#sed -i "s/add_library( armadillo/add_library( armadillo STATIC/" CMakeLists.txt
- *    	#sed -i "s/include(ARMA_FindMKL)/#include(ARMA_FindMKL)/" CMakeLists.txt
- *    mkdir build
- *    cd build
- *    cmake ..
- *    make
- *    cd ../..
- *    make
- *    sbatch ./goslurm.sh ann_mnist_digits
+      git clone git@github.com:yetanotherpassword/cosc3500 --branch="Milestone1"
+      cd ~/cosc3500/
+      unzip mnist.zip
+      unxz armadillo-10.6.2.tar.xz
+      tar xvf armadillo-10.6.2.tar
+      cd armadillo-10.6.2/
+      	#Made lib static and issue with MKL on Centos
+      	#Below changes done in my git, but may need to do if download from
+      	#http://sourceforge.net/projects/arma/files/armadillo-10.6.2.tar.xz
+      	#sed -i "s/add_library( armadillo/add_library( armadillo STATIC/" CMakeLists.txt
+      	#sed -i "s/include(ARMA_FindMKL)/#include(ARMA_FindMKL)/" CMakeLists.txt
+      mkdir build
+      cd build
+      cmake ..
+      make
+      cd ../..
+      make
+      sbatch ./goslurm.sh ann_mnist_digits
  */
 // g++ armo.cpp -g -o armo -std=c++11 -O2 -larmadillo
 
