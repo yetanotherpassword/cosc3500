32344e9ae8e3f161ed61535646e914636db40b14
Allan Campton <s4604901@fac-login-1.local>
2021-11-19 02:35:57 +1000

    Add cuda device info request at start

--- ann_mnist_digits3.cu
+++ ann_mnist_digits3.cu
@@ -1142,6 +1142,21 @@ int main(int argc, char *argv[])
 size_t available, total;
 cudaMemGetInfo(&available, &total);
 cout << "avail="<< available<< " total=" << total<<endl;
+ int nDevices;
+
+  cudaGetDeviceCount(&nDevices);
+  for (int i = 0; i < nDevices; i++) {
+    cudaDeviceProp prop;
+    cudaGetDeviceProperties(&prop, i);
+    printf("Device Number: %d\n", i);
+    printf("  Device name: %s\n", prop.name);
+    printf("  Memory Clock Rate (KHz): %d\n",
+           prop.memoryClockRate);
+    printf("  Memory Bus Width (bits): %d\n",
+           prop.memoryBusWidth);
+    printf("  Peak Memory Bandwidth (GB/s): %f\n\n",
+           2.0*prop.memoryClockRate*(prop.memoryBusWidth/8)/1.0e6);
+  }
      extern char **environ;
      string hname = "";
      //string y="initial_random_values_weights_11337071.txt";
